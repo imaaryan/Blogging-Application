724cb4e47e1d35e51860520c0f7a40f4076fd82a
imaaryan <imaaryanpandey@gmail.com>
2025-08-28 12:33:55 +0530

    NavBar Added

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { assets } from '../assets/assets.js';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
 
-export default NavBar
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 x1:mx-32">
+      <img onClick={()=>navigate("/")} src={assets.logo} alt="logo" className="w-32 sm:w-44" />
+      <button onClick={()=>navigate("/login")} className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">
+        Login
+        <img src={assets.arrow} className="w-3" alt="arrow" />
+      </button>
+    </div>
+  );
+};
+
+export default NavBar;
